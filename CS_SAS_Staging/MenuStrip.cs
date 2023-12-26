861156d5220bffdd83be36499c4d654d6b6c3c18
AXSORO <suckmydicknewyork@gmail.com>
2023-12-26 14:11:41 -0800

    Added processes for menustrip buttons

--- CS_SAS_Staging/MenuStrip.cs
+++ CS_SAS_Staging/MenuStrip.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,50 +19,54 @@ namespace CS_SAS_Staging
       //
         public static void OpenPowershell()
         {
-
+            Process.Start("powershell.exe");
         }
         public static void OpenCMD()
         {
-
+            Process.Start("cmd.exe");
         }
       //
       // "Windows" section of toolstrip
       //
         public static void LusrMgr()
         {
-
+            Process.Start("lusrmgr.msc");
         }
         public static void ControlUpdate()
         {
-
+            Process.Start("Control Update");
         }
         public static void GpEdit()
         {
-
+            Process.Start("gpedit.msc");
         }
-        public static void DisgMgmt()
+        public static void DiskMgmt()
         {
-
+            Process.Start("diskmgmt.msc");
         }
         public static void EventViewer()
         {
-
+            Process.Start("eventvwr.exe");
         }
         public static void PerfMon()
         {
-
+            Process.Start("perfmon.exe");
         }
         public static void DevMgmt()
         {
-
+            Process.Start("devmgmt.msc");
         }
         public static void MsConfig()
         {
+            Process.Start("msconfig.exe");
+        }
+        //
+        // "Help" section of toolstrip 
+        //
+        public static void HelpButton()
+        {
 
         }
-      //
-      // "Help" section of toolstrip 
-      //
-
+        
     }
 }
